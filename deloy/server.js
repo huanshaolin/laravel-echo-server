b1200a4bb037826cc7c5f9bba492977bf171298a
Hoaang Hao <hoanghaosktt1ls@gmail.com>
2020-07-16 20:00:36 +0700

    11

--- deloy/server.js
+++ deloy/server.js
@@ -1,8 +1,8 @@
 const Echo = require("./dist");
 const { constants } = require('crypto');
 var options = {
-    authHost: 'http://app:8001',
-    authEndpoint: '/auth/broadcasting',
+    authHost: process.env.AUTH_HOST,
+    authEndpoint: process.env.AUTH_ENDPOINT,
     clients: [],
     database: 'sqlite',
     databaseConfig: {
@@ -12,14 +12,15 @@ var options = {
         },
         kafka:{
             "group.id": "a830y1na-echo",
-            "metadata.broker.list": "moped-01.srvs.cloudkafka.com:9094,moped-02.srvs.cloudkafka.com:9094,moped-03.srvs.cloudkafka.com:9094",
+            "metadata.broker.list": "207.46.227.84:9092,207.46.227.84:9093,207.46.227.84:9094",
             "socket.keepalive.enable": true,
-            "security.protocol": "SASL_SSL",
-            "sasl.mechanisms": "SCRAM-SHA-256",
-            "sasl.username": "a830y1na",
-            "sasl.password": "ShGeGU7mi30Yp0tyUpYXBNmQF2kiyvBu",
             // "debug": "generic,broker,security",
             'enable.auto.commit':true,
+        },
+        rabbimmq:{
+            uri:process.env.RABBITMQ_URI,
+            queue:process.env.RABBITMQ_QUEUE,
+            durable:true
         }
     },
     devMode: true,
@@ -35,7 +36,8 @@ var options = {
     subscribers: {
         http: true,
         redis: false,
-        kafka:true,
+        kafka:false,
+        rabbitmq:true
     },
     apiOriginAllow: {
         allowCors: false,
